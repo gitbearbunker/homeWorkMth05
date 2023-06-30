ba828a3d4c41b7b83ff3b9c9cc22658da297aad9
Aleksandr <119926857+gitbearbunker@users.noreply.github.com>
2023-06-30 16:38:30 +0300

    Stub and tests for findByName

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -71,11 +71,50 @@ public class PhoneBookTest {
         //arrange
         String name = "Vasya";
         String number = "8 800 555 55 55";
-        String expectedResult = null;
 
         //act
         book.add(name, number);
         String result = book.findByNumber("8 800 555 55 57");
+
+        //assert
+        Assertions.assertEquals(null, result);
+    }
+
+    @Test
+    public void findByNameTrueTest() {
+        //arrange
+        String name = "Vasya";
+        String number = "8 800 555 55 55";
+
+        String name1 = "Petya";
+        String number1 = "8 800 555 55 55";
+
+        String expectedResult = "8 800 555 55 55";
+        //act
+        book.add(name, number);
+        book.add(name1, number1);
+
+        String result = book.findByName("Petya");
+
+        //assert
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void findByNameFalseTest() {
+        //arrange
+        String name = "Vasya";
+        String number = "8 800 555 55 55";
+
+        String name1 = "Petya";
+        String number1 = "8 800 555 55 55";
+
+        String expectedResult = "8 800 555 55 57";
+        //act
+        book.add(name, number);
+        book.add(name1, number1);
+
+        String result = book.findByName("Petya");
 
         //assert
         Assertions.assertEquals(expectedResult, result);
